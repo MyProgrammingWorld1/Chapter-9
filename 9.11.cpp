882bc877b33a86aa461b99754764e9e15618d9f1
M. Isnaan Ashraf 23511(CS) <139842251+MyProgrammingWorld1@users.noreply.github.com>
2024-08-28 01:47:18 -0700

    Update 9.11.cpp

--- 9.11.cpp
+++ 9.11.cpp
@@ -3,25 +3,25 @@
 #include <iostream>
 using namespace std;
 
-// Function declaration
+ 
 void swap(int &a, int &b);
 
 int main() {
     int num1, num2;
 
-    // Input two integers from the user
+    
     cout << "Enter the first integer: ";
     cin >> num1;
 
     cout << "Enter the second integer: ";
     cin >> num2;
 
-    // Display values before swapping
+   
     cout << "Before swapping: " << endl;
     cout << "First integer: " << num1 << endl;
     cout << "Second integer: " << num2 << endl;
 
-    // Call the function to swap the values
+    
     swap(num1, num2);
 
     // Display values after swapping
@@ -32,7 +32,7 @@ int main() {
     return 0;
 }
 
-// Function definition
+
 void swap(int &a, int &b) {
     int temp = a;
     a = b;
