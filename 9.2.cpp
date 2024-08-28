9a24835a2a316ad8d9b2bf87738d09dfd7c628e4
M. Isnaan Ashraf 23511(CS) <139842251+MyProgrammingWorld1@users.noreply.github.com>
2024-08-28 01:57:34 -0700

    Update 9.2.cpp

--- 9.2.cpp
+++ 9.2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Function to display the maximum of two numbers
+
 void displayMax(int num1, int num2) {
     if (num1 > num2) {
         cout << "The maximum number is: " << num1 << endl;
@@ -13,14 +13,12 @@ void displayMax(int num1, int num2) {
 int main() {
     int number1, number2;
 
-    // Input two numbers from the user
     cout << "Enter the first number: ";
     cin >> number1;
 
     cout << "Enter the second number: ";
     cin >> number2;
 
-    // Call the function to display the maximum number
     displayMax(number1, number2);
 
     return 0;
