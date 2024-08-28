20d348bbd925009c6943c781d8c668059dce50df
M. Isnaan Ashraf 23511(CS) <139842251+MyProgrammingWorld1@users.noreply.github.com>
2024-08-28 01:59:17 -0700

    Update 9.4.cpp

--- 9.4.cpp
+++ 9.4.cpp
@@ -2,24 +2,20 @@
 
 #include <iostream>
 using namespace std;
-
-// Function declaration
+ 
 void displayPredecessorAndSuccessor(int number);
 
 int main() {
     int num;
 
-    // Input a number from the user
     cout << "Enter a number: ";
     cin >> num;
 
-    // Call the function to display the predecessor and successor of the number
     displayPredecessorAndSuccessor(num);
 
     return 0;
 }
 
-// Function definition
 void displayPredecessorAndSuccessor(int number) {
     int predecessor = number - 1;
     int successor = number + 1;
